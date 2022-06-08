88f7c94a2377ea8cff8f99907351b8594e51a058
alejandro lopez <alejandro.lopez16@tectijuana.edu.mx>
2022-06-07 22:04:08 -0700

    Add listado de personas project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Persona } from './persona.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
-  title = 'listado-personas';
+  title = 'Listado de personas';
+
+  personas: Persona[] = [
+    new Persona('Juan', 'Perez'),
+    new Persona('Laura', 'Juarez'),
+    new Persona('Axel', 'Ramirez'),
+  ];
+
+  nombreInput: string = '';
+  apellidoInput: string = '';
+
+  agregarPersona() {
+    let persona1 = new Persona(this.nombreInput, this.apellidoInput);
+    this.personas.push(persona1);
+  }
 }
---
+++ src/app/persona/persona.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-persona',
+  templateUrl: './persona.component.html',
+  styleUrls: ['./persona.component.sass'],
+})
+export class PersonaComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
